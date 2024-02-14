9c75060a3697ea4e359ad96406489ac01929080a
Erik <81354428+X64X2@users.noreply.github.com>
2024-02-14 09:17:07 +0000

    Delete peds directory

--- peds/PedPlacement.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class CPedPlacement {
-public:
-	static void FindZCoorForPed(CVector* pos);
-	static CEntity* IsPositionClearOfCars(Const CVector*);
-	static bool IsPositionClearForPed(CVector*);
-};
