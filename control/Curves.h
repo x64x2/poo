d13296aa477fe19528ad7f4e8a0aebc54cb3e4be
Erik <81354428+X64X2@users.noreply.github.com>
2024-02-17 00:14:48 +0000

    Delete control directory

--- control/Curves.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class CVector;
-
-class CCurves
-{
-public:
-	static float CalcSpeedScaleFactor(CVector*, CVector*, float, float, float, float);
-	static void CalcCurvePoint(CVector*, CVector*, CVector*, CVector*, float, int32, CVector*, CVector*);
-};
--- control/Remote.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class CRemote
-{
-public:
-	static void GivePlayerRemoteControlledCar(float, float, float, float, uint16);
-	static void TakeRemoteControlledCarFromPlayer(void);
-};
